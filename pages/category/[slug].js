0cc2936d04a93e953bd9cbdc6d93debf81282899
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-09-03 20:59:56 +0700

    fix:typo

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -13,8 +13,8 @@ const CategoryPost = ({ posts }) => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="col-span-1 lg:col-span8">
+      <div className="grid grid-cols-1 grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
